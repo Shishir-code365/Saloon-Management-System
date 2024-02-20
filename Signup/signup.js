df833ca897db9e3368483bed20ce50c91d9fde54
Shishir-code365 <dhakal.shishir2059@gmail.com>
2024-02-20 13:08:01 +0545

    saved

---
+++ Signup/signup.js
@@ -0,0 +1,44 @@
+
+var enter = document.querySelector('.enter');
+var enteruser = document.querySelector('.enteruser');
+var enterpass = document.querySelector('.enterpass');
+var enter2 = document.querySelector('.enter2');
+var enteruser2 = document.querySelector('.enteruser2');
+var enterpass2 = document.querySelector('.enterpass2');
+var container = document.querySelector('.container');
+var container2 = document.querySelector('.container2');
+var btn = document.querySelector('.Btn');
+var btn2 = document.querySelector('.Btn2');
+
+
+enter2.addEventListener('click',function(){
+        var username2 = document.querySelector('.username2').value;
+        var password2 = document.querySelector('.password2').value;
+    if(!username2  && !password2)
+    {
+       enteruser2.style.display = 'inline';
+       enterpass2.style.display = 'inline';
+       
+    }
+    else if(!username2)
+    {
+        enteruser2.style.display = 'inline';
+        enterpass2.style.display = 'none';
+        
+    }
+    else if(!password2)
+    {
+        enterpass2.style.display = 'inline';
+        enteruser2.style.display = 'none';
+    }
+    else
+    {
+        enteruser2.style.display = 'none';
+        enterpass2.style.display = 'none';
+    }
+    })
+    
+    btn2.addEventListener('click',function(){
+        container2.style.display = 'inline';
+        container.style.display = 'none';
+    })
